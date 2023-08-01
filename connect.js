eb2efbd41ccc324a38002a0fbc240ee49ee09304
yash358 <93638045+yash358@users.noreply.github.com>
2023-08-01 15:59:31 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -2,8 +2,8 @@ import mysql from "mysql"
 import dotenv from "dotenv";
 
 export const db = mysql.createConnection({
-  host:process.env.Hostname,
-  user:"root",
+  host: process.env.Hostname,
+  user: process.env.User,
   password: process.env.Password,
   database: process.env.Database
 })
